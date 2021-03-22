a3b0eda4c9478655e8063f28c629989d2c4d6c1d
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-03-22 15:05:42 +0900

    [FEAT] : Base Activity 설정, MainActivity.kt 상속관계 변경

--- app/src/main/java/com/l2hyunwoo/searchrepoapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/l2hyunwoo/searchrepoapp/presentation/ui/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.l2hyunwoo.searchrepoapp.presentation.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.l2hyunwoo.searchrepoapp.R
+import com.l2hyunwoo.searchrepoapp.base.BindingActivity
+import com.l2hyunwoo.searchrepoapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
